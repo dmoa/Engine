550d8ea74e342aca5143102ec4eed3789eb07ada
Stan <spammyboy@protonmail.com>
2022-07-27 13:11:44 +0200

    separate itoa into its own file

--- Engine.h
+++ Engine.h
@@ -43,42 +43,7 @@ struct FloatRect {
 #define bmalloc(t) (t *)(malloc(sizeof(t)))
 #define bmalloc_arr(t, n) (t *)(malloc(sizeof(t) * n))
 
-
-#if defined(__linux__) || defined (__APPLE__)
-char *itoa(int value, char *result, int base);
-#ifdef ENGINE_IMPLEMENTATION
-// implementation of itoa found at strudel.org.uk/itoa
-char *itoa(int value, char *result, int base) {
-    // check that the base if valid
-    if (base < 2 || base > 36) {
-        *result = '\0';
-        return result;
-    }
-
-    char *ptr = result, *ptr1 = result, tmp_char;
-    int tmp_value;
-
-    do {
-        tmp_value = value;
-        value /= base;
-        *ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrst"
-                 "uvwxyz"[35 + (tmp_value - value * base)];
-    } while (value);
-
-    // Apply negative sign
-    if (tmp_value < 0)
-        *ptr++ = '-';
-    *ptr-- = '\0';
-    while (ptr1 < ptr) {
-        tmp_char = *ptr;
-        *ptr-- = *ptr1;
-        *ptr1++ = tmp_char;
-    }
-    return result;
-}
-#endif
-#endif
-
+#include "utils/itoa.h"
 #include "utils/string.h"
 
 #include <GL/glew.h>
