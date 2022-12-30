bf7dc44823dc2f02c71c26259f5d44dc2c362552
dmoa <spammyboy@protonmail.com>
2022-12-30 19:58:07 +0000

    small hack for std::string

--- utils/LoadFile.h
+++ utils/LoadFile.h
@@ -1,13 +1,38 @@
 #pragma once
 
-char *LoadFile(char *path);
+#include <string>
+
+char* LoadFile(char *path);
+char* LoadFile(std::string path);
 
 #ifdef ENGINE_IMPLEMENTATION
 
-char *LoadFile(char *path) {
+char* LoadFile(char *path) {
     FILE *file = fopen(path, "r");
-    if (!file) {
+
+    if (! file) {
         print("Couldn't load %s !", path);
+        return NULL;
+    }
+
+    // 0L because fseek takes a long integer, but we still only want zero
+    fseek(file, 0L, SEEK_END);
+    long file_size = ftell(file);
+    fseek(file, 0L, SEEK_SET);
+
+    char *buffer = (char *)calloc(file_size, sizeof(char));
+    fread(buffer, sizeof(char), file_size, file);
+
+    fclose(file);
+
+    return buffer;
+}
+
+char* LoadFile(std::string path) {
+    FILE *file = fopen(path.c_str(), "r");
+
+    if (! file) {
+        print("Couldn't load %s !", path.c_str());
         return NULL;
     }
 
